bf55e326ded467f47d68bf1fbded2a05c96541e1
Hasen Romani <hromani@uoguelph.ca>
2023-11-08 19:01:00 -0500

    Add hasenTest.php

---
+++ html/hasenTest.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Hello there Test</title>
+</head>
+<body>
+    <?php
+        // PHP code goes here
+        echo "Hello here Papi ;)!";
+    ?>
+</body>
+</html>
