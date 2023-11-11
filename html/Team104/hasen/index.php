fea6da557f3870d875767ff2dadfb301cb91eaf3
Hasen Romani <hromani@uoguelph.ca>
2023-11-11 16:01:39 -0500

    granting permissions smh

--- html/Team104/hasen/index.php
+++ html/Team104/hasen/index.php
@@ -6,7 +6,7 @@
     </head>
 <body>
 <?php  
-    $name = "Hi, I am Hasen, nice to meet you! 100👋";
+    $name = "Hi, I am Hasen, nice to meet you!👋";
     $aboutMe = "<p style=\"line-height: 2.0;\">I'm a passionate and dedicated individual with a deep love for technology and art that knows no bounds. As an avid problem solver, I thrive on challenges and enjoy exploring new ideas. With a background in Software Engineering, I'm constantly seeking opportunities to learn and grow. I'm always up for an adventure. My goal is to make a positive impact, both in my personal journey and in the lives of those around me. Join me as I navigate through the exciting twists and turns of life, always eager to discover what's next.</p>";
     $directors = "Some of my Favourite Directors 🎬";
     $movies = "My Favourite Movies by Them 🍿";
@@ -23,7 +23,6 @@
         <div class="profile-pic">
             <img class="profile" src="hasen.jpg" alt="Avatar">
         </div>
-
         <div class="intro">
             <h2 class="name"><?php echo $name; ?></h2>
         </div>
@@ -31,7 +30,6 @@
         <div class="intro2">
             <h2 class="movies"><?php echo $spacing; echo $directors; ?></h2>
         </div>
-
     </div>
 
     <br>
