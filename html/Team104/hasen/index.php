c1f3cde3ebce664804a8bbb85c121f13d9960593
Hasen Romani <hromani@uoguelph.ca>
2023-11-11 14:50:53 -0500

    hustling

--- html/Team104/hasen/index.php
+++ html/Team104/hasen/index.php
@@ -12,6 +12,7 @@
     $movies = "My Favourite Movies by Them 🍿";
     $desc = "Select a Director (or all) to See Their Best Films";
     $var = "Slay the day";
+    $var2 = "Everyday I am hustlin'";
     $spacing = "<br>"
     
 ?>  
@@ -29,6 +30,7 @@
         <div class="intro">
             <h2 class="name"><?php echo $name; ?></h2>
             <h3 class="name"><?php echo $var; ?></h2>
+            <h3 class="name"><?php echo $var2; ?></h2>
         </div>
 
         <p><?php echo $aboutMe; ?></p>
