8282fd7da0531319153f77725a3e441722a70039
Riley Deconkey <rdeconke@uoguelph.ca>
2023-11-23 19:02:50 -0500

    added credit and grade functionality.

--- html/pages/ApiFrontend/modules/calls.js
+++ html/pages/ApiFrontend/modules/calls.js
@@ -8,19 +8,22 @@ export async function getCourses() {
     xhttp.send(JSON.stringify({})); // Send POST request to url
 
     const response = JSON.parse(xhttp.responseText); // Fetch response as JS object
-    return response; // Return result
+    return response; // Return result   
 }
 
-export async function getPossibleCourses(userCourseCodes) {
-    const url = `${api_url}Course/getPossibleCourses.php`; // Get url of network request
-    const xhttp = new XMLHttpRequest(); // Make a network request
-    
-    const body = { // Define POST request body
-        coursesTaken: userCourseCodes // Add current user input courses in POST request body
-    };
 
-    xhttp.open("POST", url, false); // Define POST request
-    xhttp.send(JSON.stringify(body)); // Send POST request with body
+export async function getPossibleCourses(userCourseCodes,avg,credits) {
+   const url = `${api_url}Course/getPossibleCourses.php`; // Get url of network request
+   const xhttp = new XMLHttpRequest(); // Make a network request
+   
+   const body = { // Define POST request body
+       coursesTaken: userCourseCodes, // Add current user input courses in POST request body
+       average: avg, // send average
+       credits: credits // send total credits
+   };
 
-    return JSON.parse(xhttp.responseText); // Return response as JS object
+   xhttp.open("POST", url, false); // Define POST request
+   xhttp.send(JSON.stringify(body)); // Send POST request with body
+
+   return JSON.parse(xhttp.responseText); // Return response as JS object
 }
